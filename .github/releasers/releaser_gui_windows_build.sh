5c77ae9842519410261ff2faa5931b571910f38a
Mostafa <mostafa.sedaghat@gmail.com>
2025-06-18 22:19:06 +0800

    chore: update windows releaser

---
+++ .github/releasers/releaser_gui_windows_build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+ROOT_DIR="$(pwd)"
+BUILD_DIR="${ROOT_DIR}/build"
+
+mkdir ${PACKAGE_DIR}
+
+# Check the architecture
+ARC="$(uname -m)"
+echo "Building the binaries for Windows ${ARC} architecture"
+
+# This fixes a bug in pkgconfig: invalid flag in pkg-config --libs: -Wl,-luuid
+sed -i -e 's/-Wl,-luuid/-luuid/g' /mingw64/lib/pkgconfig/gdk-3.0.pc
+
+CGO_ENABLED=0 go build -ldflags "-s -w" -trimpath -o ${BUILD_DIR}/pactus-daemon_unsigned.exe        ./cmd/daemon
+CGO_ENABLED=0 go build -ldflags "-s -w" -trimpath -o ${BUILD_DIR}/pactus-wallet_unsigned.exe        ./cmd/wallet
+CGO_ENABLED=0 go build -ldflags "-s -w" -trimpath -o ${BUILD_DIR}/pactus-shell_unsigned.exe         ./cmd/shell
+go build -ldflags "-s -w -H windowsgui" -trimpath -tags gtk -o ${BUILD_DIR}/pactus-gui_unsigned.exe ./cmd/gtk
